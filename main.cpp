294d891c6c0917be0ae7b1a0918b3ad8ed4013c7
Vladyslav Nikitin <93128949+darkfrost72@users.noreply.github.com>
2022-02-25 19:36:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
+#include <iostream>
+#include <cstdio>
 #include "functions.h"
 
 
 int main()
 {
-	//TODO: if you want to test your functions, write your code here
-	
-	
-	
-	return 0;
+    std::setlocale(LC_ALL, "Russian");
+    float arr[3];
+    int n;
+    n=3;
+    for(int i = 0; i < 3; i++) {
+        std::cout << "Enter number" << i << "=";
+        std::cin >> arr[i];
+    }
+    findMinValue(arr);
+    return 0;
 }
