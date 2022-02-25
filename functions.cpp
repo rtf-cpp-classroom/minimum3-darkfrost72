fd8271400a03cb033c6f563c2da7192fd7211e03
Vladyslav Nikitin <93128949+darkfrost72@users.noreply.github.com>
2022-02-25 19:45:51 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -2,8 +2,16 @@
 #include <cstdio>
 #include "functions.h"
 
-float findMinValue(float arr[])
+float findMinValue()
 {
+    std::setlocale(LC_ALL, "Russian");
+    float arr[3];
+    int n;
+    n=3;
+    for(int i = 0; i < 3; i++) {
+        std::cout << "Enter number" << i << "=";
+        std::cin >> arr[i];
+    }
     float min = arr[0];
     for(int i = 0; i < 3; i++) {
         if(arr[i] < min) {
